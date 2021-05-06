b212d491ba0fbe573c1064956d0ee6c243892d2d
Mahmoud AlSofyani <msafar95@hotmail.com>
2021-05-06 23:09:54 +0400

    Added some app configurations

--- app.js
+++ app.js
@@ -18,13 +18,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET,POST,PUT,PATCH,HEAD,DELETE,OPTIONS"
+app.use(function (req, res, next) {
+  res.set(
+    "Content-Type",
+    req.accepts().includes("text/html") ? "text/html" : "application/json"
   );
-  res.header("Access-Control-Allow-Headers", "content-Type,x-requested-with");
+  res.set("Accept-Encoding", "gzip");
+  if (req.method === "OPTIONS") {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
 });
 
 app.use("/", indexRouter);
