8ff511e46ff1e531f6dc961b153ecf0722556ca7
Mahmoud AlSofyani <msafar95@hotmail.com>
2021-05-13 12:27:01 +0400

    Added search post route and controller

--- routes/posts.js
+++ routes/posts.js
@@ -3,11 +3,13 @@ const {
   createPost,
   getAllPost,
   getPostById,
+  searchPost,
 } = require("../controllers/posts-controller");
 var router = express.Router();
 
 router.get("/", getAllPost);
 router.get("/:id", getPostById);
 router.post("/", createPost);
+router.post("/search", searchPost);
 
 module.exports = router;
