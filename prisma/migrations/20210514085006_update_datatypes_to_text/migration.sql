3b530b68d416bdeee50200ec84337261c28b95e7
Mahmoud AlSofyani <msafar95@hotmail.com>
2021-05-14 12:50:27 +0400

    fixed sschema to allow text data type to work with WYSIWYG

---
+++ prisma/migrations/20210514085006_update_datatypes_to_text/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE `advertisement` MODIFY `description` TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE `event` MODIFY `details` TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE `post` MODIFY `description` TEXT NOT NULL;
