19b61875e458646c53a837aabdca0bf816723f94
Mahmoud AlSofyani <msafar95@hotmail.com>
2021-05-14 12:42:29 +0400

    new announcement email working

---
+++ helpers/emailer/announcements.js
@@ -0,0 +1,68 @@
+require("dotenv").config();
+
+const Email = require("email-templates");
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: process.env.MAILER_HOST,
+  port: process.env.MAILER_PORT,
+  secure: false,
+  auth: {
+    user: process.env.MAILER_USER,
+    pass: process.env.MAILER_PASS,
+  },
+});
+
+const attachments = [
+  {
+    filename: "dvu-logo.png",
+    path: "./public/images/dvu-logo.png",
+    cid: "duv-logo",
+  },
+  {
+    filename: "instagram-icon.png",
+    path: "./public/icons/instagram-icon.png",
+    cid: "instagram",
+  },
+  {
+    filename: "3w.png",
+    path: "./public/images/3w.png",
+    cid: "3whealthcare",
+  },
+  {
+    filename: "alnaboodah1.png",
+    path: "./public/images/alnaboodah1.png",
+    cid: "vwdubai",
+  },
+  {
+    filename: "rowe.png",
+    path: "./public/images/rowe.png",
+    cid: "rowe",
+  },
+];
+
+exports.sendNewAnnouncementEmail = (emailAddresses, title) => {
+  try {
+    const email = new Email({
+      transport: transporter,
+      send: true,
+      preview: false,
+    });
+
+    email
+      .send({
+        template: "announcement",
+        message: {
+          from: "Der Volkskreis UAE <info@volkskreisuae.com>",
+          to: emailAddresses,
+          attachments,
+        },
+        locals: {
+          title: "New Announcement",
+        },
+      })
+      .catch((err) => console.log(err));
+  } catch (err) {
+    console.log(err);
+  }
+};
---
+++ prisma/migrations/20210514083502_update_details_column_datatype/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `announcement` MODIFY `details` TEXT NOT NULL;
