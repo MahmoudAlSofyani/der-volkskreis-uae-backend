abb8cb6de00e8195f43a2b5ef5845b324aad7d4d
Mahmoud AlSofyani <msafar95@hotmail.com>
2021-05-14 16:28:20 +0400

    Reset password working

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,15 @@
 var express = require("express");
-const { loginMember, verifyValidToken } = require("../controllers/auth-controller");
+const {
+  loginMember,
+  verifyValidToken,
+  resetMemberPassword,
+  updateMembersPassword,
+} = require("../controllers/auth-controller");
 var router = express.Router();
 
 router.post("/login", loginMember);
-router.get("/verify-token", verifyValidToken)
+router.get("/verify-token", verifyValidToken);
+router.post("/members/reset-password", resetMemberPassword);
+router.post("/members/update-password", updateMembersPassword);
 
 module.exports = router;
